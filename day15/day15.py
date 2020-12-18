53d91b3de9f90ed9f866c499222f5a90552add5e
Patrick Taylor <taylor.patrick.e@gmail.com>
2020-12-18 14:18:23 -0500

    start day 15

---
+++ day15/day15.py
@@ -0,0 +1,9 @@
+def part1():
+    input_raw = []
+    with open("input_day15.txt", "r") as reader:
+        # Read and print the entire file line by line
+        for line in reader:
+            input_raw.append(line)
+    input_cleaned = []
+    for item in input_raw:
+        input_cleaned.append(item.rstrip())
